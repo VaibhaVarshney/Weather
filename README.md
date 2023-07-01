853bdce07cfe52bedc2ef2dc45781286e183b0f2
Vaibhav Varshney <varshney2vaibhav@gmail.com>
2023-07-01 20:31:18 +0530

    final

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # Weather
  
+Weather App Documentation
+Overview
+The Weather App is a web application that allows users to search for weather information of different cities. It provides real-time weather data such as temperature, humidity, cloud percentage, wind speed, and more. The app also displays weather information for major cities by default. It is built using HTML, CSS, and JavaScript, and utilizes the Weather API to fetch weather data.
+
+File Structure
+The Weather App consists of the following files:
+
+index.html - The main HTML file that contains the structure and content of the application.
+styles.css - The CSS file that defines the styles and layout of the application.
+script.js - The JavaScript file that contains the functionality and logic of the application.
+Dependencies
+The Weather App relies on the following external dependencies:
+
+Font Awesome - A popular icon library used to display icons in the application.
+Bootstrap - A CSS framework used for styling and responsive layout of the application.
+HTML Structure
+The index.html file follows the standard structure of an HTML document. It consists of several sections that define the layout and components of the Weather App:
+
+CSS Styles
+The styles.css file defines the styles for the Weather App. It includes custom styles as well as styles from the Bootstrap framework. The CSS file is responsible for the overall appearance and layout of the application.
+
+JavaScript Functionality
+The script.js file contains the JavaScript code that provides the functionality for the Weather App. It consists of several functions and event listeners to handle user interactions and fetch weather data from the Weather API.
+
+API Integration
+The Weather App integrates with the Weather API to retrieve weather data for different cities. The API is accessed using the fetch() method and requires an API key and host in the request headers. The API response is processed and the relevant weather information is displayed on the page.
+
+Usage
+To use the Weather App, follow these steps:
+
+Open the index.html file in a web browser.
+Enter a city name in the search input field.
+Click the search button or press Enter to fetch the weather data for the entered city.
+The weather information for the city will be displayed in a card format.
+By default, weather information for major cities is displayed in a table.
+Toggle the dark mode switch to change the theme of the application.
--- script.js
+++ script.js
@@ -6,60 +6,69 @@ const options = {
   },
 };
 
-
-const updateTheme = (temperature) => {
-    const body = document.body;
-  
-    if (temperature >= 28) {
-      body.style.backgroundImage = "url('summer.jpg')";
-    } else if (temperature < 10) {
-      body.style.backgroundImage = "url('winter.jpg')";
-    } else {
-      body.style.backgroundImage = "url('spring.jpg')";
-    }
-  };
-
+// Function to get weather data for a specific city
 
 const getWeather = (city) => {
-    const capitalizedCity = city.replace(/\b\w/g, function(match) {
-        return match.toUpperCase();
-      });
-      cityName.innerHTML = capitalizedCity;
-      cityName.innerHTML = capitalizedCity;
-      document.getElementById('weather-card-container').style.display = 'block';
-      fetch("https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=" + city, options)
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error("Unable to fetch weather data. Please try again.");
-          }
-          return response.json();
-        })
-        .then((response) => {
-          console.log(response);
-          cloud_pct.innerHTML = response.cloud_pct;
-          cloud_pct2.innerHTML = response.cloud_pct;
-          temp.innerHTML = response.temp;
-          temp2.innerHTML = response.temp;
-          temp3.innerHTML = response.temp;
-          feels_like.innerHTML = response.feels_like;
-          humidity.innerHTML = response.humidity;
-          min_temp.innerHTML = response.min_temp;
-          max_temp.innerHTML = response.max_temp;
-          wind_speed.innerHTML = response.wind_speed;
-          wind_speed2.innerHTML = response.wind_speed;
-          wind_degrees.innerHTML = response.wind_degrees;
-          sunrise.innerHTML = formatTime(response.sunrise);
-          sunset.innerHTML = formatTime(response.sunset);
-    
-          // Update the theme based on the temperature
-          updateTheme(parseFloat(response.temp));
-          
-        })
-        .catch((err) => {
-          console.error(err);
-          alert("An error occurred while fetching weather data. Please try again.");
-        });
-    };
+  const capitalizedCity = city.replace(/\b\w/g, function (match) {
+    return match.toUpperCase();
+  });
+  cityName.innerHTML = capitalizedCity;
+
+  document.getElementById("weather-card-container").style.display = "block";
+  fetch(
+    "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=" + city,
+    options
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Unable to fetch weather data. Please try again.");
+      }
+      return response.json();
+    })
+    .then((response) => {
+      console.log(response);
+      // Update weather data in the HTML elements
+
+      cloud_pct.innerHTML = response.cloud_pct;
+      cloud_pct2.innerHTML = response.cloud_pct;
+      temp.innerHTML = response.temp;
+      temp2.innerHTML = response.temp;
+      feels_like.innerHTML = response.feels_like;
+      feels_like2.innerHTML = response.feels_like;
+      humidity.innerHTML = response.humidity;
+      min_temp.innerHTML = response.min_temp;
+      max_temp.innerHTML = response.max_temp;
+      wind_speed.innerHTML = response.wind_speed;
+      wind_speed2.innerHTML = response.wind_speed;
+      wind_degrees.innerHTML = response.wind_degrees;
+      sunrise.innerHTML = formatTime(response.sunrise);
+      sunset.innerHTML = formatTime(response.sunset);
+
+      // Update the theme based on the temperature
+      const temperature = parseFloat(response.temp);
+      const cloudPercentage = parseInt(response.cloud_pct);
+      if (temperature > 27 && cloudPercentage <= 90) {
+        document.body.style.backgroundImage = "url('summer.jpg')";
+      } else if (temperature < 15 && temperature > 0 && cloudPercentage <= 90) {
+        document.body.style.backgroundImage = "url('winter.jpg')";
+      } else if (temperature <= 0) {
+        document.body.style.backgroundImage = "url('winter.jpg')";
+      } else if (
+        temperature >= 15 &&
+        temperature <= 35 &&
+        cloudPercentage <= 90
+      ) {
+        document.body.style.backgroundImage = "url('spring.jpg')";
+      } else if (cloudPercentage > 90) {
+        document.body.style.backgroundImage = "url('rain.jpg')";
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      alert("An error occurred while fetching weather data. Please try again.");
+    });
+};
+// Function to format a timestamp into a time string
 
 function formatTime(timestamp) {
   const time = new Date(timestamp * 1000);
@@ -68,47 +77,44 @@ function formatTime(timestamp) {
   return hours + ":" + minutes;
 }
 
-const submit = document.getElementById("submit");
-const city = document.getElementById("city");
 const cityName = document.getElementById("cityName");
-const cloud_pct = document.getElementById("cloud_pct");
-const cloud_pct2 = document.getElementById("cloud_pct2");
-const temp = document.getElementById("temp");
-const temp2 = document.getElementById("temp2");
-const temp3 = document.getElementById("temp3");
-const feels_like = document.getElementById("feels_like");
-const humidity = document.getElementById("humidity");
-const min_temp = document.getElementById("min_temp");
-const max_temp = document.getElementById("max_temp");
-const wind_speed = document.getElementById("wind_speed");
-const wind_speed2 = document.getElementById("wind_speed2");
-const wind_degrees = document.getElementById("wind_degrees");
-const sunrise = document.getElementById("sunrise");
-const sunset = document.getElementById("sunset");
+
+// Event listener for the submit button
 
 submit.addEventListener("click", (e) => {
   e.preventDefault();
   const searchCity = city.value;
   getWeather(searchCity);
 });
+// Default city and initial weather display
 
- const defaultCity = "Delhi";
- getWeather(defaultCity);
+const defaultCity = "Delhi";
+getWeather(defaultCity);
+// Array of cities to fetch weather data for
 
 const cities = ["Delhi", "Bangalore", "Pune", "Mumbai", "Jaipur", "Chennai"];
+// Fetch weather data for each city in the array
 getCitiesWeather(cities);
+// Function to fetch weather data for multiple cities
 
 function getCitiesWeather(cities) {
   cities.forEach((city) => {
-    fetch("https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=" + city, options)
+    fetch(
+      "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=" + city,
+      options
+    )
       .then((response) => {
         if (!response.ok) {
-          throw new Error(`Unable to fetch weather data for ${city}. Please try again.`);
+          throw new Error(
+            `Unable to fetch weather data for ${city}. Please try again.`
+          );
         }
         return response.json();
       })
       .then((response) => {
         console.log(response);
+        // Update weather data in the corresponding HTML row
+
         const row = document.getElementById(city);
         if (row) {
           row.innerHTML = `
@@ -128,31 +134,33 @@ function getCitiesWeather(cities) {
       })
       .catch((err) => {
         console.error(err);
-        alert(`An error occurred while fetching weather data for ${city}. Please try again.`);
+        alert(
+          `An error occurred while fetching weather data for ${city}. Please try again.`
+        );
       });
   });
 }
 
+// Function to search for a specific city
+
 function searchCity() {
-    var cityInput = document.getElementById('city').value;
-    getWeather(cityInput);
-  }
+  var cityInput = document.getElementById("city").value;
+  getWeather(cityInput);
+}
 
-  
-  
+// Dark mode toggle
 
+const darkModeToggle = document.getElementById("darkModeToggle");
+const darkModeIcon = document.querySelector(".dark-mode-icon i");
 
-  const darkModeToggle = document.getElementById('darkModeToggle');
-const darkModeIcon = document.querySelector('.dark-mode-icon i');
-
-darkModeToggle.addEventListener('change', function() {
+darkModeToggle.addEventListener("change", function () {
   if (this.checked) {
-    document.body.classList.add('dark-mode');
-    darkModeIcon.classList.remove('fa-moon');
-    darkModeIcon.classList.add('fa-sun');
+    document.body.classList.add("dark-mode");
+    darkModeIcon.classList.remove("fa-moon");
+    darkModeIcon.classList.add("fa-sun");
   } else {
-    document.body.classList.remove('dark-mode');
-    darkModeIcon.classList.remove('fa-sun');
-    darkModeIcon.classList.add('fa-moon');
+    document.body.classList.remove("dark-mode");
+    darkModeIcon.classList.remove("fa-sun");
+    darkModeIcon.classList.add("fa-moon");
   }
 });
